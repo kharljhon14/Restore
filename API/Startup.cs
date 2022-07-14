9db96d7f8daccd7c192ed7b0de672bdec520b969
Karl <enriquezkharl14@gmail.com>
2022-07-15 06:14:05 +0800

    CORS

--- API/Startup.cs
+++ API/Startup.cs
@@ -27,10 +27,13 @@ namespace API
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
             });
+
             services.AddDbContext<StoreContext>(opt =>
             {
                 opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
             });
+
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -46,6 +49,8 @@ namespace API
             // app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(opt => { opt.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");});
 
             app.UseAuthorization();
 
--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,38 +1,44 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
-  const [products, setProducts] = useState([
-    { name: "Lorem", price: 100.0 },
-    { name: "Lorem2", price: 120.0 },
-  ]);
+   const [products, setProducts] = useState([
+      { name: "Lorem", price: 100.0 },
+      { name: "Lorem2", price: 120.0 },
+   ]);
 
-  function addProducts() {
-    setProducts((prevState) => [
-      ...prevState,
-      {
-        name: `Product ${prevState.length + 1}`,
-        price: (prevState.length * 100) + 100,
-      },
-    ]);
-  }
+   useEffect(() => {
+      fetch("http://localhost:5000/api/products")
+         .then((response) => response.json())
+         .then((data) => setProducts(data));
+   }, []);
 
-  function handleProducts() {
-    addProducts();
-  }
+   function addProducts() {
+      setProducts((prevState) => [
+         ...prevState,
+         {
+            name: `Product ${prevState.length + 1}`,
+            price: prevState.length * 100 + 100,
+         },
+      ]);
+   }
 
-  return (
-    <div className="App">
-      <h1>Re-Store</h1>
-      <ul>
-        {products.map((item, index) => (
-          <li key={index}>
-            {item.name} : {item.price}
-          </li>
-        ))}
-      </ul>
-      <button onClick={handleProducts}>Add product</button>
-    </div>
-  );
+   function handleProducts() {
+      addProducts();
+   }
+
+   return (
+      <div className="App">
+         <h1>Re-Store</h1>
+         <ul>
+            {products.map((item, index) => (
+               <li key={index}>
+                  {item.name} : {item.price}
+               </li>
+            ))}
+         </ul>
+         <button onClick={handleProducts}>Add product</button>
+      </div>
+   );
 }
 
 export default App;
