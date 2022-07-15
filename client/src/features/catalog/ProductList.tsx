b5d220b636ef1874d5c2f0d0d875f1c6c9893f20
Karl <enriquezkharl14@gmail.com>
2022-07-15 23:09:37 +0800

    Product Cards

--- client/src/features/catalog/ProductList.tsx
+++ client/src/features/catalog/ProductList.tsx
@@ -1,4 +1,4 @@
-import { List, ListItem, ListItemAvatar, Avatar, ListItemText } from "@mui/material";
+import { Grid } from "@mui/material";
 import { Product } from "../../app/models/product";
 import ProductCard from "./ProductCard";
 
@@ -9,11 +9,13 @@ interface Props {
 export default function ProductList({ products }: Props) {
    return (
       <>
-         <List>
+         <Grid container spacing={4}>
             {products.map((product) => (
-               <ProductCard key={product.id} product={product} />
+               <Grid item xs={4} key={product.id}>
+                  <ProductCard product={product} />
+               </Grid>
             ))}
-         </List>
+         </Grid>
       </>
    );
 }
