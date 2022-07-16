408b26d8384e8e7152f8108249031063270b8ead
kharl jhon rhane enriquez <enriquezkharl14@gmail.com>
2022-07-16 21:51:00 +0800

    Styling

---
+++ client/src/App.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { useState } from "react";
+
+function App() {
+  const [products, setProducts] = useState([
+    { name: "Lorem", price: 100.0 },
+    { name: "Lorem2", price: 120.0 },
+  ]);
+
+  function addProducts() {
+    setProducts((prevState) => [
+      ...prevState,
+      {
+        name: `Product ${prevState.length + 1}`,
+        price: (prevState.length * 100) + 100,
+      },
+    ]);
+  }
+
+  function handleProducts() {
+    addProducts();
+  }
+
+  return (
+    <div className="App">
+      <h1>Re-Store</h1>
+      <ul>
+        {products.map((item, index) => (
+          <li key={index}>
+            {item.name} : {item.price}
+          </li>
+        ))}
+      </ul>
+      <button onClick={handleProducts}>Add product</button>
+    </div>
+  );
+}
+
+export default App;
--- client/src/features/catalog/ProductCard.tsx
+++ client/src/features/catalog/ProductCard.tsx
@@ -1,26 +1,52 @@
-import { Button, Card, CardActions, CardContent, CardMedia, Typography } from "@mui/material";
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Typography,
+  CardHeader,
+  Avatar,
+} from "@mui/material";
 import { Product } from "../../app/models/product";
 
 interface Props {
-   product: Product;
+  product: Product;
 }
 
 export default function ProductCard({ product }: Props) {
-   return (
-      <Card>
-         <CardMedia component="img" height="140" image="http://picsum.photos/200" alt="green iguana" />
-         <CardContent>
-            <Typography gutterBottom variant="h5" component="div">
-               Lizard
-            </Typography>
-            <Typography variant="body2" color="text.secondary">
-               Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica
-            </Typography>
-         </CardContent>
-         <CardActions>
-            <Button size="small">Share</Button>
-            <Button size="small">Learn More</Button>
-         </CardActions>
-      </Card>
-   );
+  return (
+    <Card>
+      <CardHeader
+        avatar={
+          <Avatar sx={{ bgcolor: "secondary.main" }}>
+            {product.name.charAt(0).toUpperCase()}
+          </Avatar>
+        }
+        title={product.name}
+        titleTypographyProps={{
+          sx: { fontWeight: "bold", color: "primary.main" },
+        }}
+      />
+      <CardMedia
+        component="img"
+        sx={{ backgroundSize: "contain", bgcolor: "primary.light" }}
+        image={product.pictureUrl}
+        title={product.name}
+        alt={product.name}
+      />
+      <CardContent>
+        <Typography gutterBottom color="secondary" variant="h5">
+          ${(product.price / 100).toFixed(2)}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          {product.brand} / {product.type}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Add to cart</Button>
+        <Button size="small">View</Button>
+      </CardActions>
+    </Card>
+  );
 }
